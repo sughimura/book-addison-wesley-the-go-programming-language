7ddea5e85dfe51a8059f53e1e5d6e61b8c0ac0be
sughimura <nxsughi@gmail.com>
2022-05-08 11:52:00 +0900

    chore(os.Stderr): add NOTE

--- ch1/fetch.go
+++ ch1/fetch.go
@@ -12,8 +12,8 @@ func main() {
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url) // エラーがなければレスポンス構造体respで結果を返す。respのBodyフィールドは読み込み可能なストリームとしてサーバからのレスポンスを含む
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1) // プロセスをステータスコード1で終了させる
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err) // 標準エラー出力にエラー内容を出力する
+			os.Exit(1)                                 // プロセスをステータスコード1で終了させる
 		}
 		b, err := ioutil.ReadAll(resp.Body) // レスポンス全体を読み込み結果をbに保存する
 		resp.Body.Close()                   // 資源のリークを防ぐためにストリームを閉じる
